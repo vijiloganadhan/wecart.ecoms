d212d5043997ebde0d140f2ebf0e13c85932da06
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-09-01 08:39:56 +0530

    new ewcart 12

--- flipcart_ecom/flipcart_ecom/settings.py
+++ flipcart_ecom/flipcart_ecom/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
     }
 }
 import dj_database_url
-DATABASES = { 'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600) }
+DATABASES['default'] = dj_database_url.parse(config("DATABASE_URL", default="sqlite:///db.sqlite3"))
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
