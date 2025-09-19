1c781f011c123d1033223204005ad0e24887e0a0
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-09-19 20:15:28 +0530

    new

--- flipcart_ecom/flipcart/models.py
+++ flipcart_ecom/flipcart/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from cloudinary import CloudinaryField
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class Category(models.Model):
     cname=models.CharField(max_length=100)
